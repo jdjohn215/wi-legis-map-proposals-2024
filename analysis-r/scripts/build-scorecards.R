14e5882c9e49fda3169e8cb2ccb54087852a5d15
jdjohn215 <jdjohn215@gmail.com>
2024-01-10 09:57:11 -0600

    expand png width

--- analysis-r/scripts/build-scorecards.R
+++ analysis-r/scripts/build-scorecards.R
@@ -219,7 +219,7 @@ senate.gt
 
 ################################################################################
 # save output
-gtsave(assembly.gt, "scorecards/assembly-scorecard.png", vwidth = 1200)
+gtsave(assembly.gt, "scorecards/assembly-scorecard.png", vwidth = 1350)
 gtsave(assembly.gt, "scorecards/assembly-scorecard.html")
-gtsave(senate.gt, "scorecards/senate-scorecard.png", vwidth = 1200)
+gtsave(senate.gt, "scorecards/senate-scorecard.png", vwidth = 1350)
 gtsave(senate.gt, "scorecards/senate-scorecard.html")
